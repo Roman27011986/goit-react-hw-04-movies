63c4c41197e3215f3866cab77490fec4823f1bb0
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-05-22 18:47:37 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
-import MoviesList from '../../components/MoviesList'
-import { getTrendsMovies } from '../../services/apiMovies'
-import styles from './HomePage.module.css'
+import MoviesList from '../../components/MoviesList';
+import { getTrendsMovies } from '../../services/apiMovies';
+import styles from './HomePage.module.css';
 
 
 
 class HomePage extends Component {
 
     state = {
-        movies: []
+        movies: [],
+        filterGenre:27011986
     };
 
     async componentDidMount() {
     try {
         const { data } = await getTrendsMovies()
-      this.setState({ movies: data.results })
+        this.setState({ movies: data.results })
     } catch (err) {
         alert(err);
         };
     };
 
+    handleOnChange = (e) => {
+        this.setState({ filterGenre: Number(e.target.value) })
+        
+    };
+
+    getVisibleMovies = () => {
+        const { movies, filterGenre } = this.state;
+        if (filterGenre === 27011986) {
+            return movies
+        };
+        return movies.filter(movie =>
+            movie.genre_ids.includes(filterGenre)
+        );
+    };
+
     render() {
-        const {movies} = this.state
+        
+        const filteredMovies = this.getVisibleMovies();
+      
         return (
             <div className={styles.wrap}>
                 <h2 className={styles.text}>Trending today</h2>
-                <MoviesList onMovies={movies} />
+
+        <select
+                    name="image"
+               onChange={this.handleOnChange}     
+        >           <option value="27011986">All</option>
+                    <option value="53">Thriller</option>
+                    <option value="18">Drama</option>
+                    <option value="28">Action</option>
+          <option value="9648">Mystery</option>
+        </select>
+                <MoviesList onMovies={filteredMovies} />
             </div>
         );
     };
